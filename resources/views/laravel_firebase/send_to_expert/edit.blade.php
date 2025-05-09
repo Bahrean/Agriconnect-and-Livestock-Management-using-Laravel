38ce13c29110125a14e120831f982ae6c1d03961
Bahrean <bahreanplusplus@gmail.com>
2025-05-09 09:17:41 +0300

    update project

--- resources/views/laravel_firebase/send_to_expert/edit.blade.php
+++ resources/views/laravel_firebase/send_to_expert/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Edit Market Price</div>
+                <div class="card-header">Edit Expert Sent</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('send_to_expert.update', $firebaseKey) }}">
@@ -67,7 +67,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Update Market Price
+                                    Update Expert Sent
                                 </button>
                             </div>
                         </div>
