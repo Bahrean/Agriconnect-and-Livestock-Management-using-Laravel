3ee44611cf105a518fe71933f45a9a921f72ee41
Bahrean <bahreanplusplus@gmail.com>
2025-05-09 09:18:01 +0300

    update project

--- resources/views/laravel_firebase/send_to_farmer/edit.blade.php
+++ resources/views/laravel_firebase/send_to_farmer/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Edit Market Price</div>
+                <div class="card-header">Edit Farmer Sent</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('market-price.update', $firebaseKey) }}">
