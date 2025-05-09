0cc2b95a27747ad93ae7893ea3daec70dca72ecb
Bahrean <bahreanplusplus@gmail.com>
2025-05-09 09:12:41 +0300

    update project

--- database/migrations/2025_05_08_111657_create_send_to_experts_table.php
+++ database/migrations/2025_05_08_111657_create_send_to_experts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('send_to_experts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('status');
             $table->timestamps();
         });
     }
