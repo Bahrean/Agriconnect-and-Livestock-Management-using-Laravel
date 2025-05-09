7bd36b4bf09e6ab91148446b45a8558d3596da9c
Bahrean <bahreanplusplus@gmail.com>
2025-05-09 09:16:34 +0300

    update project

--- resources/views/laravel_firebase/send_to_buyer/edit.blade.php
+++ resources/views/laravel_firebase/send_to_buyer/edit.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Edit Market Price</div>
+            <div class="card" style="background-color:#001B07">
+                <div class="card-header" style="background-color:#001B07">Edit Buyer Sent</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('send_to_buyer.update', $firebaseKey) }}">
@@ -15,7 +15,7 @@
                         <div class="form-group row">
                             <label for="item_name" class="col-md-4 col-form-label text-md-right">Item Name</label>
                             <div class="col-md-6">
-                                <input id="item_name" type="text" class="form-control @error('item_name') is-invalid @enderror" 
+                                <input style="background-color:#032E0E" id="item_name" type="text" class="form-control @error('item_name') is-invalid @enderror" 
                                     name="item_name" value="{{ old('item_name', $priceData['item_name'] ?? '') }}" required>
                                 @error('item_name')
                                     <span class="invalid-feedback" role="alert">
@@ -28,7 +28,7 @@
                         <div class="form-group row">
                             <label for="price" class="col-md-4 col-form-label text-md-right">Price</label>
                             <div class="col-md-6">
-                                <input id="price" type="number" step="0.01" class="form-control @error('price') is-invalid @enderror" 
+                                <input style="background-color:#032E0E" id="price" type="number" step="0.01" class="form-control @error('price') is-invalid @enderror" 
                                     name="price" value="{{ old('price', $priceData['price'] ?? 0) }}" required>
                                 @error('price')
                                     <span class="invalid-feedback" role="alert">
@@ -41,7 +41,7 @@
                         <div class="form-group row">
                             <label for="market_location" class="col-md-4 col-form-label text-md-right">Market Location</label>
                             <div class="col-md-6">
-                                <input id="market_location" type="text" class="form-control @error('market_location') is-invalid @enderror" 
+                                <input style="background-color:#032E0E" id="market_location" type="text" class="form-control @error('market_location') is-invalid @enderror" 
                                     name="market_location" value="{{ old('market_location', $priceData['market_location'] ?? '') }}" required>
                                 @error('market_location')
                                     <span class="invalid-feedback" role="alert">
@@ -54,7 +54,7 @@
                         <div class="form-group row">
                             <label for="date_recorded" class="col-md-4 col-form-label text-md-right">Date Recorded</label>
                             <div class="col-md-6">
-                                <input id="date_recorded" type="date" class="form-control @error('date_recorded') is-invalid @enderror" 
+                                <input style="background-color:#032E0E" id="date_recorded" type="date" class="form-control @error('date_recorded') is-invalid @enderror" 
                                     name="date_recorded" value="{{ old('date_recorded', $priceData['date_recorded'] ?? now()->format('Y-m-d')) }}" required>
                                 @error('date_recorded')
                                     <span class="invalid-feedback" role="alert">
@@ -67,7 +67,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Update Market Price
+                                    Update Buyer Sent
                                 </button>
                             </div>
                         </div>
