24d7e260a32a78b01787b82aa4daf811a0b3669d
Bahrean <bahreanplusplus@gmail.com>
2025-05-09 09:18:50 +0300

    update project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
           @livewireStyles
     </head>
     <body   class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900"  style="background-color:#032E0E;">
         @include('layouts.navigation')
 
             <!-- Page Content -->
