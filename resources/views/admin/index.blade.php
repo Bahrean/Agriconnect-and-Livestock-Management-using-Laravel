d979bb7385f3c85ee3065ba2a2d64c881e050079
Bahrean <bahreanplusplus@gmail.com>
2025-05-10 17:16:06 +0300

    update project

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -21,60 +21,62 @@
             <div class="card p-4 shadow" style="background-color:#001B07;">
                 <h5 class="card-title text-center mb-4">Quick Actions</h5>
                 <div class="row g-4">
+                                        <div class="col-md-4">
+                        <form action="{{ route('firebase.users.index') }}" method="GET">
+                            <button 
+                                type="submit" 
+                                class="btn btn-success btn-lg w-100 p-4 shadow-sm" 
+                                style="height: 150px; font-size: 1.25rem;">
+                                Show Registered Farmers
+                            </button>
+                        </form>
+                    </div>
+
                     <div class="col-md-4">
-                    <form action="{{ route('admin.addmember') }}" method="GET">
-                        <button 
-                            type="submit" 
-                            class="btn btn-primary btn-lg w-100 p-4 shadow-sm" 
-                            style="height: 150px; font-size: 1.25rem;">
-                            Add New Member
-                        </button>
-                    </form>
+                        <form action="{{ route('admin.addagricultureexpert') }}" method="GET">
+                            <button 
+                                type="submit" 
+                                class="btn btn-primary btn-lg w-100 p-4 shadow-sm" 
+                                style="height: 150px; font-size: 1.25rem;">
+                                Register New Agriculture Expert
+                            </button>
+                        </form>
 
                     </div>
                     <div class="col-md-4">
-                        <form action="{{ route('admin.showmember') }}" method="GET">
+                        <form action="{{ route('market-price.create') }}" method="GET">
                             @csrf
                             <button 
                                 type="submit" 
                                 class="btn btn-secondary btn-lg w-100 p-4 shadow-sm" 
                                 style="height: 150px; font-size: 1.25rem;">
-                                Edit Member Profile
+                                Insert Market Price
                             </button>
                         </form>
                     </div>
                     <div class="col-md-4">
-                        <form action="{{ route('admin.showmember') }}" method="GET">
+                        <form action="{{ route('market-price.index') }}" method="GET">
                             @csrf
                             <button 
                                 type="submit" 
                                 class="btn btn-danger btn-lg w-100 p-4 shadow-sm" 
                                 style="height: 150px; font-size: 1.25rem;">
-                                Delete Member
+                                Show Recorded Market Price
                             </button>
                         </form>
                     </div>
                     <div class="col-md-4">
-                        <form action="{{ route('admin.showmember') }}" method="GET">
+                        <form action="{{ route('chat.index') }}" method="GET">
                             @csrf
                             <button 
                                 type="submit" 
                                 class="btn btn-warning btn-lg w-100 p-4 shadow-sm" 
                                 style="height: 150px; font-size: 1.25rem;">
-                                Activate/Deactivate Member
+                                Chat
                             </button>
                         </form>
                     </div>
-                    <div class="col-md-4">
-                        <form action="{{ route('admin.showmember') }}" method="GET">
-                            <button 
-                                type="submit" 
-                                class="btn btn-success btn-lg w-100 p-4 shadow-sm" 
-                                style="height: 150px; font-size: 1.25rem;">
-                                Show All Members
-                            </button>
-                        </form>
-                    </div>
+                    
                 </div>
             </div>
         </div>
