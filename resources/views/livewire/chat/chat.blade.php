f6e2e969cbb91c5d308ad54233bfa5b99cb74e63
Bahrean <bahreanplusplus@gmail.com>
2025-05-10 17:07:47 +0300

    update project

--- resources/views/livewire/chat/chat.blade.php
+++ resources/views/livewire/chat/chat.blade.php
@@ -1,4 +1,4 @@
-<div class="fixed inset-0 flex h-full bg-white border lg:shadow-sm overflow-hidden lg:top-16 lg:inset-x-2 lg:m-auto lg:h-[90%] rounded-t-lg" style="background-color:rgb(60, 87, 145)">
+<div class="fixed inset-0 flex h-full bg-white border lg:shadow-sm overflow-hidden lg:top-16 lg:inset-x-2 lg:m-auto lg:h-[90%] rounded-t-lg" style="background-color:rgb(31, 122, 54)">
 
     <!-- Sidebar -->
     <div class="hidden lg:flex relative w-full md:w-[320px] xl:w-[400px] overflow-y-auto shrink-0 h-full border">
